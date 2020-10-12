1c618e10e304d0b055cea853a4f0c03c15829605
DeepinPrTools <zhangqiuliang@uniontech.com>
2020-10-12 17:36:07 +0800

    update

---
+++ docs/class_dtk_1_1_gui_1_1_d_window_manager_helper.js
@@ -0,0 +1,45 @@
+var class_dtk_1_1_gui_1_1_d_window_manager_helper =
+[
+    [ "MotifDecoration", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ab086957ee7de52cbf655230bdd285710", [
+      [ "DECOR_BORDER", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ab086957ee7de52cbf655230bdd285710a744fb008a63a99d370be343be9b8ccb2", null ],
+      [ "DECOR_RESIZEH", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ab086957ee7de52cbf655230bdd285710a77e5314634165cb239a769d10e0ba65a", null ],
+      [ "DECOR_TITLE", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ab086957ee7de52cbf655230bdd285710a396f143dfee82833ca5e43b00349a330", null ],
+      [ "DECOR_MENU", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ab086957ee7de52cbf655230bdd285710aa2bd50d84035ab39c22e2b0c52a7475e", null ],
+      [ "DECOR_MINIMIZE", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ab086957ee7de52cbf655230bdd285710af925551efd6d05caebd05cafb2c3d8fe", null ],
+      [ "DECOR_MAXIMIZE", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ab086957ee7de52cbf655230bdd285710a20468e932f45f95fbff41b8b8bfaa647", null ],
+      [ "DECOR_ALL", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ab086957ee7de52cbf655230bdd285710a25862a42e1cb34d08726139a9d6a94b3", null ]
+    ] ],
+    [ "MotifFunction", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ad5766aeea4bb2aa324fcb244a21514d8", [
+      [ "FUNC_RESIZE", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ad5766aeea4bb2aa324fcb244a21514d8a6fcfce4294056a747a422f39f5402f83", null ],
+      [ "FUNC_MOVE", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ad5766aeea4bb2aa324fcb244a21514d8ae90a1b6a31f14d1480f901ca0cefd1ca", null ],
+      [ "FUNC_MINIMIZE", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ad5766aeea4bb2aa324fcb244a21514d8ac4f49706c3b77aee31440df779b84b8d", null ],
+      [ "FUNC_MAXIMIZE", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ad5766aeea4bb2aa324fcb244a21514d8a92ab6a8e5362c08f41bd866c1ec1054d", null ],
+      [ "FUNC_CLOSE", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ad5766aeea4bb2aa324fcb244a21514d8aab9c94d1364b1584b2c1c3e67426701b", null ],
+      [ "FUNC_ALL", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ad5766aeea4bb2aa324fcb244a21514d8accd8f7166f08d8dd0f2c36115bd19ed7", null ]
+    ] ],
+    [ "WMName", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a5e4ff57b986788b412a6fe703560d84c", [
+      [ "OtherWM", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a5e4ff57b986788b412a6fe703560d84ca6ea2374986183e74149725e715b9570d", null ],
+      [ "DeepinWM", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a5e4ff57b986788b412a6fe703560d84ca42e951756a650977db86e974e4bd427c", null ],
+      [ "KWinWM", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a5e4ff57b986788b412a6fe703560d84ca685f9e678564da09f97b8b6bd6cedd60", null ]
+    ] ],
+    [ "~DWindowManagerHelper", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a901e769a691c37988053383a24d5fab1", null ],
+    [ "DWindowManagerHelper", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a1d91179800dc746baeda220fa1b1c773", null ],
+    [ "allWindowIdList", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a698fa6b5fea24c8041f9925942a01154", null ],
+    [ "currentWorkspaceWindowIdList", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a3e11c8763679af22cd3407618a3c001e", null ],
+    [ "currentWorkspaceWindows", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a24bbafdb8aefeb6f88fc219bc0a2e4ec", null ],
+    [ "hasBlurWindow", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#aa294a3a9770687d84a809e0a52b293ea", null ],
+    [ "hasBlurWindowChanged", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#af13cd739ca876d1289faeb56fd2f8961", null ],
+    [ "hasComposite", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a8977df439a892afdedb7e61cd50ca63d", null ],
+    [ "hasCompositeChanged", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a6c32e62be0e5c4d78e660e9473e8743c", null ],
+    [ "hasNoTitlebar", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ae6c2a30df138f842f8c79822c2fd349b", null ],
+    [ "hasNoTitlebarChanged", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ae0f817ed142d5373a7a275f4d6f83fb9", null ],
+    [ "windowFromPoint", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a536f8cac8a2b716127b13a11245f78e5", null ],
+    [ "windowListChanged", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a09a4e41ba5262bfe24ed74637b728c52", null ],
+    [ "windowManagerChanged", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a1ead94d963fc7efe1770bd9668b79927", null ],
+    [ "windowManagerName", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#ac08162df774e3f87b2b8bfbc221f39c7", null ],
+    [ "windowManagerNameString", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a65c6f84e011350f2fe563c423580a898", null ],
+    [ "windowMotifWMHintsChanged", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a82220c436bbfde11b384734b0d5602bf", null ],
+    [ "hasBlurWindow", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a2e8a5bd094194f850c14dd3f49820593", null ],
+    [ "hasComposite", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a83d7f3b1bd5db4565cd58a7a88340025", null ],
+    [ "hasNoTitlebar", "class_dtk_1_1_gui_1_1_d_window_manager_helper.html#a096e0059a9eed5672b48e794c4890c38", null ]
+];
---
+++ docs/html/class_dtk_1_1_gui_1_1_d_window_group_leader.js
@@ -0,0 +1,9 @@
+var class_dtk_1_1_gui_1_1_d_window_group_leader =
+[
+    [ "DWindowGroupLeader", "class_dtk_1_1_gui_1_1_d_window_group_leader.html#a0e71db51a4fe60ccff014013512b5498", null ],
+    [ "~DWindowGroupLeader", "class_dtk_1_1_gui_1_1_d_window_group_leader.html#a14256c594688308f18c94022ffa2b57b", null ],
+    [ "addWindow", "class_dtk_1_1_gui_1_1_d_window_group_leader.html#a716ea1c91101e3acc85400461b159623", null ],
+    [ "clientLeaderId", "class_dtk_1_1_gui_1_1_d_window_group_leader.html#ace0bd2d93873d0f4fef7bdf5f5e80eb4", null ],
+    [ "groupLeaderId", "class_dtk_1_1_gui_1_1_d_window_group_leader.html#a76357cd436ab64bc7be1165697a00175", null ],
+    [ "removeWindow", "class_dtk_1_1_gui_1_1_d_window_group_leader.html#af61049c9987e44cda3de34508d913fba", null ]
+];
